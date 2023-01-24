c2b11988133b08bafaab341448bcedaf557f10a0
antonioDurval <antoniodurvalc@gmail.com>
2023-01-24 15:36:40 +0000

    alteração no script para envio dos logs para o nfs montado

--- shellscripts/statusapache.sh
+++ shellscripts/statusapache.sh
@@ -3,7 +3,7 @@ DATAHORA=$(date '+%d/%m/%Y %H:%M:%S')
 STATUS=$(systemctl status httpd)
 
 if [[ $STATUS == *"active (running)"* ]]; then
-	echo "${DATAHORA} - HTTPD, ONLINE, O Apache está ativo" | tee -a /shellscripts/apache_on.txt
+	echo "${DATAHORA} - HTTPD, ONLINE, O Apache está ativo" | tee -a /nfs/antonio/apache_log_on.txt
 else
-	echo "${DATAHORA} - HTTPD, OFFLINE, O Apache não está ativo" | tee -a /shellscripts/apache_off.txt
+	echo "${DATAHORA} - HTTPD, OFFLINE, O Apache não está ativo" | tee -a /nfs/antonio/apache_log_off.txt
 fi
